4326f1ae49ba459f993690b16011e802ad1df39a
Sebastien Iooss <sebastien.iooss@student.ecp.fr>
2015-10-23 13:33:47 +0200

    add install base file

---
+++ install_base.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p 'Input the new user name : ' name
+echo creating $name
+adduser $name
+usermod -a -G sudo,ev3dev,plugdev,audio,video,input,bluetooth,i2c $name
+
+echo 'updating'
+apt-get update
+apt-get upgrade
+apt-get dist-upgrade
+
+echo 'finish'
+
